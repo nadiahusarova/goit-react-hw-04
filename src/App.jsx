16878e5a6c8c66aa2c90fc0753a8f601b9f38e9e
nadiahusarova <ngusarova911@gmail.com>
2024-08-25 21:51:56 +0300

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";  
 import SearchBar from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
 import axios from "axios";
 import "./App.css";
 
@@ -9,11 +10,13 @@ const ACCESS_KEY = "9vhbMiaLrWG-vmsc6FvETUigSWziqEPsqlj9Ebk_5bk";
 export default function App() {
   const [images, setImages] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!searchTerm) return;
 
     const fetchImages = async () => {
+      setIsLoading(true); // Показати лоадер перед запитом
       try {
         const response = await axios.get(
           `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${ACCESS_KEY}`
@@ -21,6 +24,8 @@ export default function App() {
         setImages(response.data.results);
       } catch (error) {
         console.error("Error fetching images", error);
+      } finally {
+        setIsLoading(false); 
       }
     };
 
@@ -39,6 +44,7 @@ export default function App() {
     <div>
       <SearchBar onSubmit={handleSearchSubmit} />
       <ImageGallery items={images} onImageClick={handleImageClick} />
+      {isLoading && <Loader />} 
     </div>
   );
 }
