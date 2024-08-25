6e1a2fa88e71e1601d5272f7a3d93164d94005a6
nadiahusarova <ngusarova911@gmail.com>
2024-08-25 22:07:46 +0300

    add error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import React, { useState, useEffect } from "react";  
+import React, { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
 import axios from "axios";
 import "./App.css";
 
-const ACCESS_KEY = "9vhbMiaLrWG-vmsc6FvETUigSWziqEPsqlj9Ebk_5bk"; 
+const ACCESS_KEY = "9vhbMiaLrWG-vmsc6FvETUigSWziqEPsqlj9Ebk_5bk";
 
 export default function App() {
   const [images, setImages] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null); 
 
   useEffect(() => {
     if (!searchTerm) return;
 
     const fetchImages = async () => {
-      setIsLoading(true); // Показати лоадер перед запитом
+      setIsLoading(true);
+      setError(null); 
       try {
         const response = await axios.get(
           `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${ACCESS_KEY}`
@@ -24,8 +27,9 @@ export default function App() {
         setImages(response.data.results);
       } catch (error) {
         console.error("Error fetching images", error);
+        setError("OOOPS! Something went wrong. Try again later."); 
       } finally {
-        setIsLoading(false); 
+        setIsLoading(false);
       }
     };
 
@@ -43,8 +47,14 @@ export default function App() {
   return (
     <div>
       <SearchBar onSubmit={handleSearchSubmit} />
-      <ImageGallery items={images} onImageClick={handleImageClick} />
-      {isLoading && <Loader />} 
+      {error ? (
+        <ErrorMessage /> 
+      ) : (
+        <>
+          <ImageGallery items={images} onImageClick={handleImageClick} />
+          {isLoading && <Loader />} 
+        </>
+      )}
     </div>
   );
 }
