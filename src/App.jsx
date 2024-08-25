419c750c9458d5255241ccbc2cafedbb67405b23
nadiahusarova <ngusarova911@gmail.com>
2024-08-25 22:50:10 +0300

    add loadMoreBtn?, change collor in searchBar cmpnt

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import Loader from "./components/Loader/Loader";
 import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
 import axios from "axios";
 import "./App.css";
 
@@ -12,48 +13,59 @@ export default function App() {
   const [images, setImages] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null); 
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     if (!searchTerm) return;
 
     const fetchImages = async () => {
       setIsLoading(true);
-      setError(null); 
+      setError(null);
+
       try {
         const response = await axios.get(
-          `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${ACCESS_KEY}`
+          `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${ACCESS_KEY}&page=${page}`
         );
-        setImages(response.data.results);
+        setImages((prevImages) => [...prevImages, ...response.data.results]);
+        setTotalPages(response.data.total_pages);
       } catch (error) {
-        console.error("Error fetching images", error);
-        setError("OOOPS! Something went wrong. Try again later."); 
+        setError("OOOPS! Something went wrong. Try again later.");
       } finally {
         setIsLoading(false);
       }
     };
 
     fetchImages();
-  }, [searchTerm]);
+  }, [searchTerm, page]);
 
   const handleSearchSubmit = (term) => {
     setSearchTerm(term);
+    setImages([]);
+    setPage(1);
   };
 
   const handleImageClick = (image) => {
     console.log("Image clicked:", image);
   };
 
+  const loadMoreImages = () => {
+    if (page < totalPages) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
   return (
     <div>
       <SearchBar onSubmit={handleSearchSubmit} />
-      {error ? (
-        <ErrorMessage /> 
-      ) : (
-        <>
-          <ImageGallery items={images} onImageClick={handleImageClick} />
-          {isLoading && <Loader />} 
-        </>
+      {isLoading && <Loader />}
+      {error && <ErrorMessage message={error} />}
+      {images.length > 0 && (
+        <ImageGallery items={images} onImageClick={handleImageClick} />
+      )}
+      {images.length > 0 && !isLoading && page < totalPages && (
+        <LoadMoreBtn onClick={loadMoreImages} />
       )}
     </div>
   );
